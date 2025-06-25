4e6abae5e2f56c1685767234bbe0cac52eacb9fc
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-25 23:23:48 +0530

    modified:   bun.lock 	modified:   package.json 	modified:   src/app/(dashboard)/agents/page.tsx 	modified:   src/app/layout.tsx 	new file:   src/constants.ts 	new file:   src/modules/agents/hooks/use-agents-filters.tsx 	new file:   src/modules/agents/params.ts 	modified:   src/modules/agents/server/procedures.ts 	modified:   src/modules/agents/ui/components/agent-form.tsx 	modified:   src/modules/agents/ui/components/agents-list-header.tsx 	new file:   src/modules/agents/ui/components/agents-search-filter.tsx 	modified:   src/modules/agents/ui/components/columns.tsx 	new file:   src/modules/agents/ui/components/data-pagination.tsx 	modified:   src/modules/agents/ui/views/agents-view.tsx

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import {
   AgentsView,
@@ -9,19 +10,30 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const AgentsPage = async () => {
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const AgentsPage = async ({ searchParams }: Props) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
   if (!session) redirect("/auth/sign-in");
 
+  const filters = await loadSearchParams(searchParams);
+
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
   return (
     <>
