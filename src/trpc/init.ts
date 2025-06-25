8e4efaf730c42eb0f4ca231238580233d4e5782f
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-25 17:37:13 +0530

    modified:   src/trpc/init.ts

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,6 +1,13 @@
 import { auth } from '@/lib/auth';
 import { TRPCError, initTRPC } from '@trpc/server';
 import { headers } from 'next/headers';
+import { cache } from 'react';
+export const createTRPCContext = cache(async () => {
+  /**
+   * @see: https://trpc.io/docs/server/context
+   */
+  return { userId: 'user_123' };
+});
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
